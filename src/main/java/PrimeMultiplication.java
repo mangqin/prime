50e5c6eaadf12637c730034998ec2069731f1b14
Mang Qin <mangq@yahoo.com>
2016-05-10 09:25:55 -0700

    check for non integer inputs

--- src/main/java/PrimeMultiplication.java
+++ src/main/java/PrimeMultiplication.java
@@ -1,20 +1,37 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author mangqin
+ *
+ */
 public class PrimeMultiplication {
 
+	/**
+	 * main method to find prime numbers and print multiplication table
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int n = 1;
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the number of prime numbers you want or type 0 to quit: ");
-		n = in.nextInt();
+		
 		while (n >= 1) {
-			ArrayList<Integer> numbers = PrimePopulate.populate(n);
-			PrintMultiplicationTable.printTable(numbers);
-			System.out.println("Enter the number of prime numbers you want or type 0 to quit: ");
-			n = in.nextInt();
-
+			try {
+				String s = in.next();
+				n = Integer.valueOf(s);
+				ArrayList<Integer> numbers = PrimePopulate.populate(n);
+				PrintMultiplicationTable.printTable(numbers);
+				System.out.println("Enter the number of prime numbers you want or type 0 to quit: ");
+				
+	
+			}
+			catch (NumberFormatException  exp) {
+				System.out.println("Please enter a valid number: ");
+			}
 		}
+		
 	}
 
 }
